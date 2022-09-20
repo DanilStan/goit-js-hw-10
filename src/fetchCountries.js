c2801729ed693a5a69686753dd069f88d9dbb6e3
DanilStan <stanislavov.danik@gmail.com>
2022-09-20 15:34:36 +0300

    Update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,10 +3,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v2/name/${name}?fullText=true,fields=name,capital,population,languages,flags`
-  ).then(r => {
-    if (r.status === 404) {
-      Notify.failure('Oops, there is no country with that name');
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error();
+      // Notify.failure('Oops, there is no country with that name')
     }
-    return r.json();
+    return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function onInput(event) {
         makcupList(value);
       })
       .catch(error => {
-        console.log(error);
+        Notify.failure('Oops, there is no country with that name');
       });
   }
 }
@@ -37,41 +37,59 @@ function makcupList(value) {
     );
   }
 
-  for (let i = 0; i < value.length; i += 1) {
-    const status = {
-      name: value[i].name,
-      capital: value[i].capital,
-      population: value[i].population,
-      flags: value[i].flags.svg,
-      languages: value[i].languages,
-    };
-    status.languages = status.languages[0].nativeName;
+  //   for (let i = 0; i < value.length; i += 1) {
+  //     const status = {
+  //       name: value[i].name,
+  //       capital: value[i].capital,
+  //       population: value[i].population,
+  //       flags: value[i].flags.svg,
+  //       languages: value[i].languages,
+  //     };
+  //     status.languages = status.languages[0].nativeName;
 
-    if (value.length > 1 && value.length < 10) {
-      infoMarkup(status);
-      resetFunc(countryInfo);
-    } else if (value.length === 1) {
-      cardMarkup(status);
-      resetFunc(countryList);
-    }
+  if (value.length > 1 && value.length < 10) {
+    console.log(value);
+    countryList.insertAdjacentHTML('beforeend', infoMarkup(value));
+    resetFunc(countryInfo);
+  } else if (value.length === 1) {
+    console.log(value);
+    countryInfo.insertAdjacentHTML('beforeend', cardMarkup(value));
+    resetFunc(countryList);
   }
 }
+// }
 
-function infoMarkup({ name, flags }) {
-  countryList.insertAdjacentHTML(
-    'beforeend',
-    `<li class="info__item"><img src="${flags}" alt="Country Flag" class="info__image"><span>${name}</span></li>`
-  );
+function infoMarkup(value) {
+  return value
+    .map(({ flags, name }) => {
+      return `<li class="info__item"><img src="${flags.svg}" alt="Country Flag" class="info__image"><span>${name}</span></li>`;
+    })
+    .join('');
+  //   countryList.insertAdjacentHTML(
+  //     'beforeend',
+  //     `<li class="info__item"><img src="${flags}" alt="Country Flag" class="info__image"><span>${name}</span></li>`
+  //   );
 }
 
-function cardMarkup({ name, capital, popupaltion, flags, languages }) {
-  countryInfo.insertAdjacentHTML(
-    'beforeend',
-    `<ul class="card__list">
-        <li class="card__item"><img class="card__img" src="${flags}" alt="Country Flag"></img><span class="card__text"><b> ${name}</b></span></li>
+function cardMarkup(value) {
+  return value
+    .map(({ flags, capital, population, languages, name }) => {
+      const languagesArr = languages.map(value => value.name);
+      return `<ul class="card__list">
+        <li class="card__item"><img class="card__img" src="${flags.svg}" alt="Country Flag"></img><span class="card__text"><b> ${name}</b></span></li>
         <li class="list__item"><span><b>Capital:</b> ${capital}</span></li>
-        <li class="list__item"><span><b>Population</b>: ${popupaltion}</span></li>
-        <li class="list__item"><span><b>Languages</b>: ${languages}</span></li>
-  </ul>`
-  );
+        <li class="list__item"><span><b>Population</b>: ${population}</span></li>
+        <li class="list__item"><span><b>Languages</b>: ${languagesArr}</span></li>
+  </ul>`;
+    })
+    .join('');
+  //   countryInfo.insertAdjacentHTML(
+  //     'beforeend',
+  //     `<ul class="card__list">
+  //         <li class="card__item"><img class="card__img" src="${flags}" alt="Country Flag"></img><span class="card__text"><b> ${name}</b></span></li>
+  //         <li class="list__item"><span><b>Capital:</b> ${capital}</span></li>
+  //         <li class="list__item"><span><b>Population</b>: ${popupaltion}</span></li>
+  //         <li class="list__item"><span><b>Languages</b>: ${languages}</span></li>
+  //   </ul>`
+  //   );
 }
